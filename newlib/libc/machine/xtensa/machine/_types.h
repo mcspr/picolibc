746e787eb3e5b60b41c586f3049459a65bae1ece
Maxim Prokhorov <prokhorov.max@outlook.com>
2025-07-01 19:34:12 +0300

    correct chain for types

--- newlib/libc/machine/xtensa/machine/_types.h
+++ newlib/libc/machine/xtensa/machine/_types.h
@@ -4,6 +4,17 @@
 
 #ifndef _MACHINE__TYPES_H
 #define _MACHINE__TYPES_H
+
 #include <machine/_default_types.h>
-#define IN_ADDR_T_DEFINED 1 // For LWIP2
+// avoid gcc provided types, preserve signatures & printf formatting
+// ref. newlib-xtensa#08a150b17c55de2c038d24697d1d2b73f3d0612f which replaces _default_types.h
+#undef __INT16_TYPE__
+#define __INT16_TYPE__ short
+#undef __INT32_TYPE__
+#define __INT32_TYPE__ int
+#undef __INT64_TYPE__
+#define __INT64_TYPE__ long long
+
+#include <machine/_default_types.h>
+
 #endif
---
+++ newlib/libc/machine/xtensa/machine/types.h
@@ -0,0 +1,9 @@
+#ifndef _SYS_TYPES_H
+#error "must be included via <sys/types.h>"
+#endif /* !_SYS_TYPES_H */
+
+#ifdef _IN_ADDR_T_DECLARED
+#ifndef IN_ADDR_T_DEFINED
+#define IN_ADDR_T_DEFINED 1 // For LWIP2
+#endif
+#endif
